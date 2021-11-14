672f2ef4da743cc9b7a84c5a8a7f7355f7be8c40
Kasra <kasramc2015@gmail.com>
2021-11-14 19:14:42 +0330

    i made this change like a whole 2 months ago and forgor to push it :skull: - Input now uses a single scanner instead of two.

--- src/core.java
+++ src/core.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class core {
     public static void main(String[] args) {
-        Scanner input1 = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Please input your first number");
-        double input1double = input1.nextDouble();
-        Scanner input2 = new Scanner(System.in);
+        double input1double = input.nextDouble();
         System.out.println("Please input your second number");
-        double input2double = input2.nextDouble();
-        Scanner operation = new Scanner(System.in);
+        double input2double = input.nextDouble();
         System.out.println("Please state what to do with the inputted numbers");
         System.out.println("Current possible operations are: Add(+), Subtract(-), Multiply(*), Divide(/)");
+        Scanner operation = new Scanner(System.in);
         String operationstr = operation.nextLine();
         double result;
+        double leftover;
         switch (operationstr) {
             case "Add", "+", "add" -> result = input1double + input2double;
             case "Subtract", "-", "subtract" -> result = input1double - input2double;
