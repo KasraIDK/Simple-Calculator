ee9052c26c97eff19d2dcc7023dd52fecc75a65e
Kasra <kasramc2015@gmail.com>
2021-11-16 11:17:46 +0330

    - Added "Raise to" as an operation - Added "Nth root" as an operation

--- src/core.java
+++ src/core.java
@@ -8,7 +8,9 @@ public class core {
         System.out.println("Please input your second number");
         double input2double = input.nextDouble();
         System.out.println("Please state what to do with the inputted numbers");
-        System.out.println("Current possible operations are: Add(+), Subtract(-), Multiply(*), Divide(/)");
+        System.out.println("Current possible operations are: Add(+), Subtract(-), Multiply(*), Divide(/), Raise to(^)†, Nth Root(//)††");
+        System.out.println("† 'Raise to' will raise your first number to your second");
+        System.out.println("†† N = Your second input");
         Scanner operation = new Scanner(System.in);
         String operationstr = operation.nextLine();
         double result;
@@ -17,6 +19,19 @@ public class core {
             case "Subtract", "-", "subtract" -> result = input1double - input2double;
             case "Multiply", "*", "multiply" -> result = input1double * input2double;
             case "Divide", "/", "divide" -> result = input1double / input2double;
+            case "Raise", "^", "raise" -> result = Math.pow(input1double, input2double);
+            case "Root", "//", "root", "nth root", "Nth Root" -> {
+                double n = 1/input2double;
+                result = Math.round(Math.pow(input1double, n));
+            }
+            case "gcd", "GCD", "gcf", "GCF" -> {
+                int input1int = (int) input1double;
+                int input2int = (int) input2double;
+                int prime;
+                int number;
+                int test = (int) Math.pow(25, 0.5);
+                result = test;
+            }
             default -> {
                 System.out.println("That operation is not supported, please try again.");
                 return;
